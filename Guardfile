9716fb5f08b232061b869f5c6648e552d79dc7df
JC <jorge.e.colindres@gmail.com>
2013-11-20 16:39:05 -0500

    initial commit

---
+++ Guardfile
@@ -0,0 +1,9 @@
+# A sample Guardfile
+# More info at https://github.com/guard/guard#readme
+
+guard 'livereload' do
+  watch(%r{^spec/javascripts/.*/(.*)\.js})
+  watch(%r{^spec/javascripts/(.*)\.js})
+  watch(%r{^javascripts/.*/(.*)\.js})
+  watch(%r{^javascripts/(.*)\.js})
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewBusApp::Application.config.secret_token = 'b729c01ecbdd71f4c9cd19e8c4bf4bd4d28852b4e8d525834b7746a64750a034505b60c7e76c99d6af43a5edc152f02fc7da83fcbd732c73af4667711e857b90'
